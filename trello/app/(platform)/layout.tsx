1164b9d8eb509030a617e5762f556413b87d0b20
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-11-26 19:54:54 +0800

    Update layout.tsx

--- trello/app/(platform)/layout.tsx
+++ trello/app/(platform)/layout.tsx
@@ -3,3 +3,5 @@ import { ClerkProvider } from "@clerk/nextjs";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return <ClerkProvider>{children}</ClerkProvider>;
 };
+
+export default PlatformLayout;
