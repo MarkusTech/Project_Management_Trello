a3390e9cf99ac7df416e603c76f77ec4c7d4f33b
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2023-11-26 20:12:44 +0800

    Update middleware.ts

--- trello/middleware.ts
+++ trello/middleware.ts
@@ -3,7 +3,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/"] // so the index localhost 3000 can be visible. if there is no public routes the landing page will be the clerk
+});
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
